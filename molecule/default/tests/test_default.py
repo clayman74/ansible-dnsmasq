022e2210a28bb5eb001c6c82a683ffbd2161280c
Kirill Sumorokov <sumorokov.k@gmail.com>
2020-01-10 11:58:38 +0500

    Add some tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,9 +7,16 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_config_file(host):
+    f = host.file('/etc/dnsmasq.conf')
 
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_resolve_domain(host):
+    cmd = host.run('nslookup dev.example.com localhost')
+
+    assert 'Name:	dev.example.com' in cmd.stdout
+    assert 'Address: 127.0.0.1' in cmd.stdout
